c442720ab8017eb2ec6bf5c989abb933b6f7d6d0
Esteban Ceron <estebance@Estebans-MacBook-Air.local>
2020-03-20 16:46:24 -0500

    [FEATURE] include firebase configuration

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import firebase from 'firebase'
 
 import App from './App.vue'
 import router from './router/index.js'
@@ -11,6 +12,21 @@ import store from './store'
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.use(VueRouter)
+
+var firebaseConfig = {
+        apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+        authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.VUE_APP_FIREBASE_AUTH_DATABASE_URL,
+        projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.VUE_APP_FIREBASE_APP_ID,
+        measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
+    }
+    // activate the configuration
+firebase.initializeApp(firebaseConfig);
+
+Vue.prototype.$firebase = firebase;
 
 Vue.config.productionTip = false
 
