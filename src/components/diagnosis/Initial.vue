e7cdc08ef7e37b669cc78f7f04bd4f0aa88642c9
Esteban Ceron <estebance@Estebans-MacBook-Air.local>
2020-03-21 15:56:52 -0500

    [HOTIX] fix media queries for recommendations and symptoms

--- src/components/diagnosis/Initial.vue
+++ src/components/diagnosis/Initial.vue
@@ -1,175 +1,145 @@
 <template>
-  <!--
   <div class="container panel">
-    <div class="wrapper">
-      <b-modal  title="Términos y condiciones"
-                size="lg"
-                v-model="largeModal"
-                @ok="largeModal = false"
-                variant="primary"
-                header-bg-variant="primary"
-                content-class="border-primary">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </b-modal>
-    </div>
-
-    <section>
-
-      <h1>¿No se siente bien?</h1>
-      <p>Al llenar este formulario, podrá determinar si usted es un posible caso SOSPECHOSO de coronavirus.</p>
-
-      <b-row>
-        <b-col sm="12" md="6" lg="5">
-          <div class="card">
-            <div class="card-header">
-              1. Síntomas
-            </div>
-            <div class="card-body">
-              <form>
-                <div class="form-check">
-                  <input type="checkbox" class="form-check-input" id="fever">
-                  <label class="form-check-label" for="fever">Fiebre mayor a 38 grados °C, durante los últimos 2 días</label>
-                </div>
-                <div class="form-check">
-                  <input type="checkbox" class="form-check-input" id="cough">
-                  <label class="form-check-label" for="cough">Tos seca</label>
-                </div>
-                <div class="form-check">
-                  <input type="checkbox" class="form-check-input" id="pain">
-                  <label class="form-check-label" for="pain">Dolor de garganta</label>
-                </div>
-                <div class="form-check">
-                  <input type="checkbox" class="form-check-input" id="breath">
-                  <label class="form-check-label" for="breath">Fatiga o dificultad respiratoria</label>
-                </div>
-                <div class="form-check">
-                  <input type="checkbox" class="form-check-input" id="tired">
-                  <label class="form-check-label" for="tired">Cansancio o malestar general</label>
-                </div>
-                <div class="form-check">
-                  <input type="checkbox" class="form-check-input" id="diarrhea">
-                  <label class="form-check-label" for="diarrhea">Diarrea</label>
-                </div>
-              </form>
-            </div>
-          </div>
-
-          <br>
-
-          <div class="card">
-            <div class="card-header">
-              2. Historial epidemiológico
-            </div>
-            <div class="card-body">
-               <form>
-                <div class="form-check">
-                  <input type="checkbox" class="form-check-input" id="abroad">
-                  <label class="form-check-label" for="abroad">¿Ha estado en el exterior durante los últimos 14 días?</label>
-                </div>
-                <div class="form-check">
-                  <input type="checkbox" class="form-check-input" id="employee">
-                  <label class="form-check-label" for="employee">¿Eres trabajador asistenial de salud?</label>
-                </div>
-                <div class="form-check">
-                  <input type="checkbox" class="form-check-input" id="hadContact">
-                  <label class="form-check-label" for="hadContact">¿Ha estado en contacto con una persona diagnósticada con COVID-19 o que haya
-                    tenido síntomas gripales y haya viajado al extranjero?</label>
-                </div>
-              </form>
-            </div>
-          </div>
-        </b-col>
-
-        <b-col sm="12" md="6">
-          <div class="card">
-            <div class="card-header">
-              3. Datos de contacto
-            </div>
-            <div class="card-body">
-              <form>
-                <div class="form-group">
-                  <label for="name"> <small id="emailHelp" class="form-text text-muted">Nombres y apellidos</small></label>
-                  <input type="text" class="form-control" id="name" placeholder="Nombres y apellidos">
-                </div>
-
-                <b-row>
-                  <b-col sm="4" md="4" lg="6">
-                    <div class="form-group">
-                      <label for="phone"><small id="emailHelp" class="form-text text-muted">Celular</small></label>
-                      <input type="Number" class="form-control" id="phone" placeholder="Celular">
-                    </div>
-                  </b-col>
-                  <b-col sm="4" md="4" lg="3">
-                    <div class="form-group">
-                      <label for="age"> <small id="emailHelp" class="form-text text-muted">Edad</small></label>
-                      <input type="Number" class="form-control" id="age" placeholder="Edad">
-                    </div>
-                  </b-col>
-                  <b-col>
-                    <small class="form-text text-muted">Sexo</small>
-                    <div class="form-check">
-                      <input class="form-check-input" type="radio" name="exampleRadios" id="female" value="woman" checked>
-                      <label class="form-check-label" for="female">
-                        Mujer
-                      </label>
-                    </div>
-                    <div class="form-check">
-                      <input class="form-check-input" type="radio" name="exampleRadios" id="male" value="man">
-                      <label class="form-check-label" for="male">
-                        Hombre
-                      </label>
-                    </div>
-                  </b-col>
-                </b-row>
-
-                <div class="form-group">
-                  <label for="email"><small id="emailHelp" class="form-text text-muted">Correo electrónico</small></label>
-                  <input type="email" class="form-control" id="email" placeholder="Correo electrónico">
-                </div>
-
-                <b-row>
-                  <b-col sm="4" md="4" lg="6">
-                    <div class="form-group">
-                      <label for="city"><small id="emailHelp" class="form-text text-muted">Ciudad</small></label>
-                      <input type="text" class="form-control" id="city" placeholder="Ciudad">
-                    </div>
-                  </b-col>
-                  <b-col sm="4" md="4" lg="6">
-                    <div class="form-group">
-                      <label for="neighborhood"><small id="emailHelp" class="form-text text-muted">Barrio</small></label>
-                      <input type="text" class="form-control" id="neighborhood" placeholder="Barrio">
-                    </div>
-                  </b-col>
-                </b-row>
-              </form>
-            </div>
-          </div>
-        </b-col>
-      </b-row>
-      <hr>
-      <b-row>
-        <b-col sm="6" md="4" lg="3">
-          <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="gridCheck">
-            <label class="form-check-label" for="gridCheck">
-              <a href="">Aceptar términos y condiciones</a>
-              <button type="submit" class="btn btn-link" @click="largeModal = true">Términos y condiciones</button>
-            </label>
-          </div>
-        </b-col>
-        <b-col sm="6" md="4" lg="4">
-          <button type="submit" class="btn btn-primary" @click="largeModal = true">Registrar información</button>
-        </b-col>
-      </b-row>
-    </section>
+    <h1>Building this feature</h1>
+    <!-- <section> -->
+    <!--   <h1 class="title">¿No se siente bien?</h1> -->
+    <!--   <p>Al llenar este formulario, podras determinar si eres es un posible caso de coronavirus.</p> -->
+    <!--   <form> -->
+    <!--     <b-row> -->
+    <!--       <b-col sm="12" md="6" lg="5"> -->
+    <!--         <div class="card"> -->
+    <!--           <div class="card-header"> -->
+    <!--             1. Síntomas -->
+    <!--           </div> -->
+    <!--           <div class="card-body"> -->
+    <!--               <div class="form-check"> -->
+    <!--                 <input type="checkbox" class="form-check-input" id="fever"> -->
+    <!--                 <label class="form-check-label" for="fever">Fiebre mayor a 38 grados °C, durante los últimos 2 días</label> -->
+    <!--               </div> -->
+    <!--               <div class="form-check"> -->
+    <!--                 <input type="checkbox" class="form-check-input" id="cough"> -->
+    <!--                 <label class="form-check-label" for="cough">Tos seca</label> -->
+    <!--               </div> -->
+    <!--               <div class="form-check"> -->
+    <!--                 <input type="checkbox" class="form-check-input" id="pain"> -->
+    <!--                 <label class="form-check-label" for="pain">Dolor de garganta</label> -->
+    <!--               </div> -->
+    <!--               <div class="form-check"> -->
+    <!--                 <input type="checkbox" class="form-check-input" id="breath"> -->
+    <!--                 <label class="form-check-label" for="breath">Fatiga o dificultad respiratoria</label> -->
+    <!--               </div> -->
+    <!--               <div class="form-check"> -->
+    <!--                 <input type="checkbox" class="form-check-input" id="tired"> -->
+    <!--                 <label class="form-check-label" for="tired">Cansancio o malestar general</label> -->
+    <!--               </div> -->
+    <!--               <div class="form-check"> -->
+    <!--                 <input type="checkbox" class="form-check-input" id="diarrhea"> -->
+    <!--                 <label class="form-check-label" for="diarrhea">Diarrea</label> -->
+    <!--               </div> -->
+    <!--           </div> -->
+    <!--         </div> -->
+    <!--         <br> -->
+    <!--         <div class="card"> -->
+    <!--           <div class="card-header"> -->
+    <!--             2. Historial epidemiológico -->
+    <!--           </div> -->
+    <!--           <div class="card-body"> -->
+    <!--               <div class="form-check"> -->
+    <!--                 <input type="checkbox" class="form-check-input" id="abroad"> -->
+    <!--                 <label class="form-check-label" for="abroad">¿Ha estado en el exterior durante los últimos 14 días?</label> -->
+    <!--               </div> -->
+    <!--               <div class="form-check"> -->
+    <!--                 <input type="checkbox" class="form-check-input" id="employee"> -->
+    <!--                 <label class="form-check-label" for="employee">¿Eres trabajador asistenial de salud?</label> -->
+    <!--               </div> -->
+    <!--               <div class="form-check"> -->
+    <!--                 <input type="checkbox" class="form-check-input" id="hadContact"> -->
+    <!--                 <label class="form-check-label" for="hadContact">¿Ha estado en contacto con una persona diagnósticada con COVID-19 o que haya -->
+    <!--                   tenido síntomas gripales y haya viajado al extranjero?</label> -->
+    <!--               </div> -->
+    <!--           </div> -->
+    <!--         </div> -->
+    <!--       </b-col> -->
+    <!--       <b-col sm="12" md="6"> -->
+    <!--         <div class="card"> -->
+    <!--           <div class="card-header"> -->
+    <!--             3. Datos de contacto -->
+    <!--           </div> -->
+    <!--           <div class="card-body"> -->
+    <!--               <div class="form-group"> -->
+    <!--                 <label for="name"> <small id="emailHelp" class="form-text text-muted">Nombres y apellidos</small></label> -->
+    <!--                 <input type="text" class="form-control" id="name" placeholder="Nombres y apellidos"> -->
+    <!--               </div> -->
+    <!--               <b-row> -->
+    <!--                 <b-col sm="4" md="4" lg="6"> -->
+    <!--                   <div class="form-group"> -->
+    <!--                     <label for="phone"><small id="emailHelp" class="form-text text-muted">Celular</small></label> -->
+    <!--                     <input type="Number" class="form-control" id="phone" placeholder="Celular"> -->
+    <!--                   </div> -->
+    <!--                 </b-col> -->
+    <!--                 <b-col sm="4" md="4" lg="3"> -->
+    <!--                   <div class="form-group"> -->
+    <!--                     <label for="age"> <small id="emailHelp" class="form-text text-muted">Edad</small></label> -->
+    <!--                     <input type="Number" class="form-control" id="age" placeholder="Edad"> -->
+    <!--                   </div> -->
+    <!--                 </b-col> -->
+    <!--                 <b-col> -->
+    <!--                   <small class="form-text text-muted">Sexo</small> -->
+    <!--                   <div class="form-check"> -->
+    <!--                     <input class="form-check-input" type="radio" name="exampleRadios" id="female" value="woman" checked> -->
+    <!--                     <label class="form-check-label" for="female"> -->
+    <!--                       Mujer -->
+    <!--                     </label> -->
+    <!--                   </div> -->
+    <!--                   <div class="form-check"> -->
+    <!--                     <input class="form-check-input" type="radio" name="exampleRadios" id="male" value="man"> -->
+    <!--                     <label class="form-check-label" for="male"> -->
+    <!--                       Hombre -->
+    <!--                     </label> -->
+    <!--                   </div> -->
+    <!--                 </b-col> -->
+    <!--               </b-row> -->
+    <!--               <div class="form-group"> -->
+    <!--                 <label for="email"><small id="emailHelp" class="form-text text-muted">Correo electrónico</small></label> -->
+    <!--                 <input type="email" class="form-control" id="email" placeholder="Correo electrónico"> -->
+    <!--               </div> -->
+    <!--               <b-row> -->
+    <!--                 <b-col sm="4" md="4" lg="6"> -->
+    <!--                   <div class="form-group"> -->
+    <!--                     <label for="city"><small id="emailHelp" class="form-text text-muted">Ciudad</small></label> -->
+    <!--                     <input type="text" class="form-control" id="city" placeholder="Ciudad"> -->
+    <!--                   </div> -->
+    <!--                 </b-col> -->
+    <!--                 <b-col sm="4" md="4" lg="6"> -->
+    <!--                   <div class="form-group"> -->
+    <!--                     <label for="neighborhood"><small id="emailHelp" class="form-text text-muted">Barrio</small></label> -->
+    <!--                     <input type="text" class="form-control" id="neighborhood" placeholder="Barrio"> -->
+    <!--                   </div> -->
+    <!--                 </b-col> -->
+    <!--               </b-row> -->
+    <!--           </div> -->
+    <!--         </div> -->
+    <!--       </b-col> -->
+    <!--     </b-row> -->
+    <!--     <hr> -->
+    <!--     <b-row> -->
+    <!--       <b-col sm="6" md="4" lg="3"> -->
+    <!--         <div class="form-check"> -->
+    <!--           <input class="form-check-input" type="checkbox" id="gridCheck"> -->
+    <!--           <label class="form-check-label" for="gridCheck"> -->
+    <!--             <a href="" target="blank">Aceptar términos y condiciones</a> -->
+    <!--           </label> -->
+    <!--         </div> -->
+    <!--       </b-col> -->
+    <!--     </b-row> -->
+    <!--     <b-row class="submit-container"> -->
+    <!--         <b-col sm="12" md="12" lg="12"> -->
+    <!--           <button type="submit" class="btn btn-primary submit-button" @click.prevent="submitForm">Registrar información</button> -->
+    <!--         </b-col> -->
+    <!--     </b-row> --> 
+    <!--   </form> -->
+    <!-- </section> -->
   </div>
-  -->
-  <h1>We are building this feature</h1>
 </template>
 
 <script>
@@ -178,7 +148,20 @@ export default {
   name: 'InitialDiagnosis',
   data(){
     return{
-      largeModal: false,
+      temperature: null, 
+      cough: null, 
+      sore: null, 
+      breathing:  null, 
+      fatigue: null, 
+      diarrhea: null,
+      travel: null, 
+      health_worker: null, 
+      personal_contact: null,
+      name: null, 
+      phone_number:  null, 
+      city: null, 
+      age: null,
+      neighborhood: null 
     }
   }
 }
@@ -186,7 +169,23 @@ export default {
 </script>
 
 <style>
-.panel{
-  background-color: #ebedef;
-}
+
+  .title{ 
+    padding: 1rem; 
+  }
+
+  .panel{
+    background-color: #ebedef;
+  }
+  
+  .submit-container{
+    padding: 1rem; 
+    margin: 1rem;
+  }
+
+  .submit-button{
+    margin: auto; 
+    display: block; 
+  }
+
 </style>
--- src/components/pages/Recommendations.vue
+++ src/components/pages/Recommendations.vue
@@ -99,7 +99,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
   .main-page{
     margin-top: 1rem;
@@ -117,27 +117,23 @@ export default {
       padding: 0.4rem 0.2rem;
       text-align: center;
   }
+
   .card-deck{
     margin: 0 auto;
     display: block;
   }
-    @media (max-width: 1024px) {
-    .col-1, .col-2, .col-3, .col-4, .col-5, .col-6, .col-7, .col-8, .col-9, .col-10, .col-11, .col-12, .col, .col-auto, .col-sm-1, .col-sm-2, .col-sm-3, .col-sm-4, .col-sm-5, .col-sm-6, .col-sm-7, .col-sm-8, .col-sm-9, .col-sm-10, .col-sm-11, .col-sm-12, .col-sm, .col-sm-auto, .col-md-1, .col-md-2, .col-md-3, .col-md-4, .col-md-5, .col-md-6, .col-md-7, .col-md-8, .col-md-9, .col-md-10, .col-md-11, .col-md-12, .col-md, .col-md-auto, .col-lg-1, .col-lg-2, .col-lg-3, .col-lg-4, .col-lg-5, .col-lg-6, .col-lg-7, .col-lg-8, .col-lg-9, .col-lg-10, .col-lg-11, .col-lg-12, .col-lg, .col-lg-auto, .col-xl-1, .col-xl-2, .col-xl-3, .col-xl-4, .col-xl-5, .col-xl-6, .col-xl-7, .col-xl-8, .col-xl-9, .col-xl-10, .col-xl-11, .col-xl-12, .col-xl, .col-xl-auto{
-      position: relative;
-      width: 50%;
-      padding-right: 0px;
-      padding-left: 0px;
-    }
+    
+  @media (max-width: 1024px) {
     .navbar-brand{
       font-size: 0.8rem;
     }
     .card-img-top{
-    padding: 0.2rem 0rem 0.2rem 0rem;
-    height:5rem;
-    width: 5rem;
-    margin-right: auto;
-    margin-left: auto;
-    display: block
+      padding: 0.2rem 0rem 0.2rem 0rem;
+      height:5rem;
+      width: 5rem;
+      margin-right: auto;
+      margin-left: auto;
+      display: block
     }
     .card-body{
       padding: 0.4rem;
@@ -168,22 +164,18 @@ export default {
   }
 
   @media (max-width: 480px) {
-    .col-1, .col-2, .col-3, .col-4, .col-5, .col-6, .col-7, .col-8, .col-9, .col-10, .col-11, .col-12, .col, .col-auto, .col-sm-1, .col-sm-2, .col-sm-3, .col-sm-4, .col-sm-5, .col-sm-6, .col-sm-7, .col-sm-8, .col-sm-9, .col-sm-10, .col-sm-11, .col-sm-12, .col-sm, .col-sm-auto, .col-md-1, .col-md-2, .col-md-3, .col-md-4, .col-md-5, .col-md-6, .col-md-7, .col-md-8, .col-md-9, .col-md-10, .col-md-11, .col-md-12, .col-md, .col-md-auto, .col-lg-1, .col-lg-2, .col-lg-3, .col-lg-4, .col-lg-5, .col-lg-6, .col-lg-7, .col-lg-8, .col-lg-9, .col-lg-10, .col-lg-11, .col-lg-12, .col-lg, .col-lg-auto, .col-xl-1, .col-xl-2, .col-xl-3, .col-xl-4, .col-xl-5, .col-xl-6, .col-xl-7, .col-xl-8, .col-xl-9, .col-xl-10, .col-xl-11, .col-xl-12, .col-xl, .col-xl-auto{
-      position: relative;
-      width: 100%;
-      padding-right: 0px;
-      padding-left: 0px;
-    }
+    
     .navbar-brand{
       font-size: 0.8rem;
     }
+    
     .card-img-top{
-    padding: 0.2rem 0rem 0.2rem 0rem;
-    height:6rem;
-    width: 6rem;
-    margin-right: auto;
-    margin-left: auto;
-    display: block
+      padding: 0.2rem 0rem 0.2rem 0rem;
+      height:6rem;
+      width: 6rem;
+      margin-right: auto;
+      margin-left: auto;
+      display: block
     }
     .card-body{
       padding: 0.1rem;
@@ -214,19 +206,13 @@ export default {
   }
 
   @media (max-width: 320px) {
-     .col-1, .col-2, .col-3, .col-4, .col-5, .col-6, .col-7, .col-8, .col-9, .col-10, .col-11, .col-12, .col, .col-auto, .col-sm-1, .col-sm-2, .col-sm-3, .col-sm-4, .col-sm-5, .col-sm-6, .col-sm-7, .col-sm-8, .col-sm-9, .col-sm-10, .col-sm-11, .col-sm-12, .col-sm, .col-sm-auto, .col-md-1, .col-md-2, .col-md-3, .col-md-4, .col-md-5, .col-md-6, .col-md-7, .col-md-8, .col-md-9, .col-md-10, .col-md-11, .col-md-12, .col-md, .col-md-auto, .col-lg-1, .col-lg-2, .col-lg-3, .col-lg-4, .col-lg-5, .col-lg-6, .col-lg-7, .col-lg-8, .col-lg-9, .col-lg-10, .col-lg-11, .col-lg-12, .col-lg, .col-lg-auto, .col-xl-1, .col-xl-2, .col-xl-3, .col-xl-4, .col-xl-5, .col-xl-6, .col-xl-7, .col-xl-8, .col-xl-9, .col-xl-10, .col-xl-11, .col-xl-12, .col-xl, .col-xl-auto{
-      position: relative;
-      width: 50%;
-      padding-right: 0px;
-      padding-left: 0px;
-    }
     .card-img-top{
-    padding: 0.2rem;
-    height:3rem;
-    width: 3rem;
-    margin-right: auto;
-    margin-left: auto;
-    display: block
+      padding: 0.2rem;
+      height:3rem;
+      width: 3rem;
+      margin-right: auto;
+      margin-left: auto;
+      display: block
     }
     .card-body{
       padding: 0.1rem;
